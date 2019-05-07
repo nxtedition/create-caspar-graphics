6cc2803fb24d88e8bf3560c5f93b0a38f7c95bf7
Robert Nagy <ronagy@icloud.com>
2019-05-07 15:18:36 +0200

    fix: removed unused requires

--- packages/caspar-graphics/config/webpack.config.dev.js
+++ packages/caspar-graphics/config/webpack.config.dev.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 const nodePath = require('./env').nodePath
 const paths = require('./paths')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
