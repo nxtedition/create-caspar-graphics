6b17948ed2cc77b19d020b302d141a4a95c96787
brucedk <40279859+brucedk@users.noreply.github.com>
2020-05-29 04:39:58 -0300

    Add webp to webpack url loader (#40)

--- packages/caspar-graphics/config/webpack.config.prod.js
+++ packages/caspar-graphics/config/webpack.config.prod.js
@@ -41,7 +41,7 @@ const createConfig = (template, dotenv) => ({
         ]
       },
       {
-        test: /\.(jpe?g|png|webm|ttf|eot|svg|otf|woff(2)?|mp4)(\?[a-z0-9=&.]+)?$/,
+        test: /\.(jpe?g|png|webm|webp|ttf|eot|svg|otf|woff(2)?|mp4)(\?[a-z0-9=&.]+)?$/,
         use: [
           {
             loader: 'url-loader',
