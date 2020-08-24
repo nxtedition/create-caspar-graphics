7ca98ef1824a0669835c30dd4476916e6ef0f863
Tobias Andersen <j.tobiasandersen@gmail.com>
2020-08-24 14:37:24 +0200

    fix(caspar-graphics): compat previewData

--- packages/caspar-graphics/src/template/create.dev.js
+++ packages/caspar-graphics/src/template/create.dev.js
@@ -14,11 +14,19 @@ export default (async function() {
 
   const isClassComponent = typeof Template.prototype?.render === 'function'
 
-  if (isClassComponent && Template.previewDataList) {
-    console.warn(
-      '[Caspar Graphics] `static previewDataList` will be removed in a future version. Move it to a named export, e.g. `export const previewData = {}`.'
-    )
-    window.previewData = Template.previewDataList
+  // COMPAT: Preview Data
+  if (isClassComponent && !previewData) {
+    if (Template.previewDataList) {
+      console.warn(
+        '[Caspar Graphics] `static previewDataList` will be removed in a future version. Move it to a named export, e.g. `export const previewData = {}`.'
+      )
+      window.previewData = Template.previewDataList
+    } else if (Template.previewData) {
+      console.warn(
+        '[Caspar Graphics] `static previewData` will be removed in a future version. Move it to a named export, e.g. `export const previewData = {}`.'
+      )
+      window.previewData = Template.previewData
+    }
   }
 
   createTemplate(Template)
