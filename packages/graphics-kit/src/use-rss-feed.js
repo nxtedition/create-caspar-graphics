6602bf7b61f5c4620bef23933cc01f0bef1fb070
Tobias Andersen <j.tobiasandersen@gmail.com>
2020-09-06 18:29:35 +0200

    feat(graphics-kit): add useRssFeed

---
+++ packages/graphics-kit/src/use-rss-feed.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Parser from 'rss-parser'
+
+export const useRssFeed = url => {
+  const [data, setData] = React.useState(null)
+
+  React.useEffect(() => {
+    if (!url) {
+      setData(null)
+      return
+    }
+
+    let isDisposed = false
+
+    async function fetchData() {
+      try {
+        const data = await new Parser().parseURL(url)
+
+        if (!isDisposed) {
+          setData(data)
+        }
+      } catch (err) {
+        console.error(`Unabel to get data from ${url}`, err)
+      }
+    }
+
+    fetchData()
+
+    return () => {
+      isDisposed = true
+    }
+  }, [url])
+
+  return data
+}
