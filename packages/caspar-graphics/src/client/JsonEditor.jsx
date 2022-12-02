b8c598874ee6dbdbb0e6ec3e8d7ff5fd2b02069b
Tobias Andersen <j.tobiasandersen@gmail.com>
2022-12-02 10:46:59 +0100

    remove unused import

--- packages/caspar-graphics/src/client/JsonEditor.jsx
+++ packages/caspar-graphics/src/client/JsonEditor.jsx
@@ -1,6 +1,5 @@
 import React, { forwardRef, useEffect, useRef, useState, useImperativeHandle } from 'react'
 import { loader } from '@monaco-editor/react'
-import Editor, { useMonaco } from '@monaco-editor/react'
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
 import * as monaco from 'monaco-editor'
