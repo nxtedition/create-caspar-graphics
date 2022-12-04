20589d504fd6daabdf0f290d60a7344f4b46de8f
Tobias Andersen <j.tobiasandersen@gmail.com>
2022-12-04 21:06:05 +0100

    examples

--- packages/create-caspar-graphics/template-react-framer-motion/templates/example/index.jsx
+++ packages/create-caspar-graphics/template-react-framer-motion/templates/example/index.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import { render, useCasparData } from '@nxtedition/graphics-kit'
+import { render, FramerMotion, useCasparData } from '@nxtedition/graphics-kit'
+import { motion } from 'framer-motion'
+import './style.css'
 
-function Example() {
-  const { text } = useCasparData()
+const FramerMotionExample = () => {
+  const { name } = useCasparData()
 
-  return <div>Example: {text}</div>
+  return (
+    <FramerMotion hide={!name}>
+      <motion.div
+        key={name}
+        className="container"
+        initial={{
+          opacity: 0,
+          y: 50,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0
+        }}
+        exit={{
+          opacity: 0,
+          y: 50
+        }}
+      >
+        {name}
+      </motion.div>
+    </FramerMotion>
+  )
 }
 
-render(Example)
+render(FramerMotionExample)
