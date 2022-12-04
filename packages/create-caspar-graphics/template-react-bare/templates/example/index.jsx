24d74f8fd79ab49c0987e64e7b9f1f39c2e84c33
Tobias Andersen <j.tobiasandersen@gmail.com>
2022-12-04 22:33:19 +0100

    more examples

--- packages/create-caspar-graphics/template-react-bare/templates/example/index.jsx
+++ packages/create-caspar-graphics/template-react-bare/templates/example/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { render, useCasparData } from '@nxtedition/graphics-kit'
+import { render, useCaspar } from '@nxtedition/graphics-kit'
 
 function Example() {
-  const { text } = useCasparData()
+  const { data, isPlaying } = useCasparData()
 
-  return <div>Example: {text}</div>
+  return (
+    <div
+      style={{
+        opacity: isPlaying ? 1 : 0,
+        position: 'absolute',
+        bottom: 80,
+        left: 266,
+        width: 1388,
+        padding: 20,
+        backgroundColor: 'white',
+        borderRadius: 6,
+        fontSize: 70,
+        fontFamily: 'Arial',
+        whiteSpace: 'nowrap',
+        display: 'flex',
+        alignItems: 'center',
+        transition: 'opacity 300ms ease-in-out'
+      }}
+    >
+      {data.name}
+    </div>
+  )
 }
 
 render(Example)
