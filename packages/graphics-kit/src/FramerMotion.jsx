ad6fadc835ec9879bfaa6813f2de2368df3d3bcf
Tobias Andersen <j.tobiasandersen@gmail.com>
2022-12-19 15:15:37 +0100

    only call safeToRemove after stop

--- packages/graphics-kit/src/FramerMotion.jsx
+++ packages/graphics-kit/src/FramerMotion.jsx
@@ -3,10 +3,10 @@ import { AnimatePresence } from 'framer-motion'
 import { useCaspar } from './use-caspar'
 
 export const FramerMotion = ({ children, hide, mode = 'wait' }) => {
-  const { isPlaying, safeToRemove } = useCaspar()
+  const { isPlaying, safeToRemove, isStopped } = useCaspar()
 
   return (
-    <AnimatePresence mode={mode} onExitComplete={safeToRemove}>
+    <AnimatePresence mode={mode} onExitComplete={isStopped ? safeToRemove : null}>
       {!hide && isPlaying ? children : null}
     </AnimatePresence>
   )
