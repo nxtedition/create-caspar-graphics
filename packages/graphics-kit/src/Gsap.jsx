99aa5697229d475e0c979d4bf01726d2bf20e924
Tobias Andersen <j.tobiasandersen@gmail.com>
2023-08-16 14:43:33 +0200

    feat(gsap): expose timeline as ref

--- packages/graphics-kit/src/Gsap.jsx
+++ packages/graphics-kit/src/Gsap.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState, forwardRef, useImperativeHandle } from 'react'
 import { gsap } from 'gsap'
 import { States } from './constants'
 import { useCaspar } from './use-caspar'
 
-export const GsapTimeline = ({ children, hide, wait = hide, onPlay, onStop }) => {
+export const GsapTimeline = forwardRef(function GsapTimeline({ children, hide, wait = hide, onPlay, onStop }, forwardedRef) {
   const [timeline] = useState(gsap.timeline({ paused: true }))
   const { state, isStopped, safeToRemove } = useCaspar()
   const [didShow, setDidShow] = useState(false)
   const readyToShow = state >= States.playing && !wait
+
+  useImperativeHandle(forwardedRef, () => timeline)
 
   useLayoutEffect(() => {
     if (!readyToShow) {
@@ -41,4 +43,4 @@ export const GsapTimeline = ({ children, hide, wait = hide, onPlay, onStop }) =>
   }, [isStopped, safeToRemove])
 
   return readyToShow || didShow ? children : null
-}
+})
