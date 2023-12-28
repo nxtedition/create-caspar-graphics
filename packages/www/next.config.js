e912853bd4de153680beab30fe4cc961d8b3fa87
Tobias Andersen <j.tobiasandersen@gmail.com>
2023-12-28 15:40:28 +0100

    fix(wwww): update next

--- packages/www/next.config.js
+++ packages/www/next.config.js
@@ -1,18 +1,15 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx'
+  themeConfig: './theme.config.tsx',
 })
 
 module.exports = withNextra({
-  experimental: {
-    appDir: true
-  },
   async rewrites() {
     return [
       {
         source: '/examples/:path*',
-        destination: 'https://caspar-graphics-examples.vercel.app/:path*'
-      }
+        destination: 'https://caspar-graphics-examples.vercel.app/:path*',
+      },
     ]
-  }
+  },
 })
